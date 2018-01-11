fc2211a0e249c6909f0c2c257f7bb725f700dd47
msalitu <maria.salgadoi@alumnos.upm.es>
2018-01-11 16:47:27 +0100

    labo

--- bd2017_2018/exercise9/my_app/app.R
+++ bd2017_2018/exercise9/my_app/app.R
@@ -1,3 +1,6 @@
+# install.packages("jsonlite")
+
+library(jsonlite)
 library(shiny)
 
 # Define UI for app that draws a histogram ----
@@ -34,14 +37,6 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
   output$distPlot <- renderPlot({
     
     x    <- faithful$waiting
--- bd2017_2018/exercise9/my_app/loadDataset.R
+++ bd2017_2018/exercise9/my_app/loadDataset.R
@@ -1,31 +1,48 @@
 
+ # install.packages("stringr")
+ #  library(stringr)
 
-library(jsonlite)
+#  install.packages("jsonlite")
+#  library(jsonlite)
 
 # load data from json file
 
-review <- stream_in(file("./data/review.json"))
-tip <- stream_in(file("./data/tip.json"))
-business <- stream_in(file("./data/business.json"))
-user <- stream_in(file("./data/user.json"))
-checkin <- stream_in(file("./data/checkin.json"))
-photos <- stream_in(file("./data/photos.json"))
+ # review <- stream_in(file("./data/review.json"))
+ # tip <- stream_in(file("./data/tip.json"))
+ # business <- stream_in(file("./data/business-trunk.json"))
+ # user <- stream_in(file("./data/user.json"))
+ # checkin <- stream_in(file("./data/checkin.json"))
+ # photos <- stream_in(file("./data/photos.json"))
 
 # flat data
 
-review_flat <- flatten(review)
-tip_flat <- flatten(tip)
-business_flat <- flatten(business)
-user_flat <- flatten(user)
-checkin_flat <- flatten(checkin)
-photos_flat <- flatten(photos)
+ # review_flat <- flatten(review)
+ # tip_flat <- flatten(tip)
+ # business_flat <- flatten(business)
+ # user_flat <- flatten(user)
+ # checkin_flat <- flatten(checkin)
+ # photos_flat <- flatten(photos)
 
 # transform into a data frame
 
-review_tbl <- as.data.frame(review_flat)
-tip_tbl <- as.data.frame(tip_flat)
-business_tbl <- as.data.frame(business_flat)
-user_tbl <- as.data.frame(user_flat)
-checkin_tbl <- as.data.frame(checkin_flat)
-photos_tbl <- as.data.frame(photos_flat)
+ # review_tbl <- as.data.frame(review_flat)
+ # tip_tbl <- as.data.frame(tip_flat)
+ # business_tbl <- as.data.frame(business_flat)
+ # user_tbl <- as.data.frame(user_flat)
+ # checkin_tbl <- as.data.frame(checkin_flat)
+ # photos_tbl <- as.data.frame(photos_flat)
 
+business <- stream_in(file("./data/business-trunk.json"))
+
+
+biz_dat <- fromJSON(sprintf("[%s]", paste(readLines("./data/business.json"), collapse=",")))
+
+restaurants<-grep(pattern="Restaurants",biz_dat$categories)
+
+bars<-biz_rest<-biz_dat[restaurants,]
+
+bizrates<-data.frame(biz_rest$business_id, biz_rest$stars,
+                     biz_rest$longitude,biz_rest$latitude, 
+                     biz_rest$state,
+                     biz_rest$attributes$`Take-out`,biz_rest$attributes$`Takes Reservations`,
+                     biz_rest$attributes$`Wi-Fi`,biz_rest$attributes$Caters)
