2ce919e49e5b8df3f58cee3031e0a7e84ae9947e
msalitu <mariasalgadoitu@gmail.com>
2018-01-15 18:42:05 +0100

    loadDataset.R funcionando

--- bd2017_2018/exercise9/my_app/loadDataset.R
+++ bd2017_2018/exercise9/my_app/loadDataset.R
@@ -6,30 +6,32 @@ library(jsonlite)
 
 bus_dat <- fromJSON(sprintf("[%s]", paste(readLines("./data/business.json"), collapse=",")))
 
-#create a table of categories:
+# there are many business categories: hotels, services...
 cats<-bus_dat$categories
 listcats<-unlist(cats)
 allcat<-table(listcats)
 category_table<-as.data.frame(allcat)
 
-#we want all related with tourism: restaurants and hotels
-toMatch <- c("Restaurants", "Hotels")
-matches <- unique (grep(paste(toMatch,collapse="|"), bus_dat$categories, value=TRUE))
+# we are just interested in restaurants
+restaurants<-grep(pattern="Restaurants",bus_dat$categories)
 
-bars<-bus_rest<-bus_dat[matches,]
+bars<-bus_rest<-bus_dat[restaurants,]
+busrates <- data.frame(bus_rest$business_id,
+                        bus_rest$stars,
+                        bus_rest$longitude,
+                        bus_rest$latitude,
+                        bus_rest$state,
+                        bus_rest$attributes$RestaurantsTakeOut,
+                        bus_rest$attributes$RestaurantsReservations,
+                        bus_rest$attributes$WiFi,
+                        bus_rest$attributes$Caters,
+                        bus_rest$attributes$RestaurantsPriceRange2,
+                        bus_rest$attributes$RestaurantsGoodForGroups,
+                        bus_rest$attributes$OutdoorSeating,
+                        bus_rest$attributes$Smoking)
 
-birates <- data.frame(bus_rest$business_id, 
-                      bus_rest$stars,
-                      bus_rest$longitude, 
-                      bus_rest$latitude,
-                      bus_rest$state, 
-                      bus_rest$attributes$RestaurantsTakeOut, 
-                      bus_rest$attributes$RestaurantsReservations,
-                      bus_rest$attributes$WiFi, bus_rest$attributes$Caters, 
-                      bus_rest$categories)
-
-cc<-complete.cases(birates)
-business<-birates[cc,]
+cc<-complete.cases(busrates)
+business<-busrates[cc,]
 write.table(business,"business.dat")
 
 
